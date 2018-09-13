9cb8f33c9a05ed8728c48a519d0bc56c24c04c9a
Nareg Najarian <najariannareg@gmail.com>
2018-09-13 22:12:47 +0400

    first commit

---
+++ hw2.js
@@ -0,0 +1,20 @@
+// Constants:
+alert("Hi first project")
+const a = 2;
+const b = 5;
+
+const firstName = 'Nareg';
+const lastName = 'Najarian';
+
+// Arithmetic Operators:
+const c = a + b;
+const d = a * b;
+
+// Boolean, Conditions if/else block:
+if (c === d){
+  console.log ('Something');
+} else if(c > d){
+  console.log ('Something Else');
+} else {
+	console.log (firstName + ' ' + lastName);
+};
